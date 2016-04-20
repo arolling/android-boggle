3454c10b356170637bc1dd8324eba445b2832e9b
Anna Lawley and Abigail Rolling <alawley1@gmail.com>
2016-04-20 09:24:06 -0700

    add intent to navigate to game activity

--- app/src/main/java/com/epicodus/bogglesolitaire/MainActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/MainActivity.java
@@ -1,13 +1,30 @@
 package com.epicodus.bogglesolitaire;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.buttonStartGame) Button mStartGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mStartGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
